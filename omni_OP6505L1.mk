4197b50ba05d68cdf80a8eccf06a6a83da96007a
andruha5530-creator <andruha5530@gmail.com>
2026-09-20 22:30:48 +0300

    Switch OrangeFox product name to twrp target

--- omni_OP6505L1.mk
+++ omni_OP6505L1.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/OP6505L1/device.mk)
 
 PRODUCT_DEVICE := OP6505L1
-PRODUCT_NAME := omni_OP6505L1
+PRODUCT_NAME := twrp_OP6505L1
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus Nord CE 6
 PRODUCT_MANUFACTURER := oneplus
