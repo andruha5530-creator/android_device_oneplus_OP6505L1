8383d2a3d89832aa09960dcc909a73bb7fd75820
andruha5530-creator <andruha5530@gmail.com>
2026-09-20 21:08:07 +0300

    Fix OP6505L1 product makefile

--- omni_OP6505L1.mk
+++ omni_OP6505L1.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/oneplus/OP6505L1/device.mk)
 
 PRODUCT_DEVICE := OP6505L1
 PRODUCT_NAME := omni_OP6505L1
