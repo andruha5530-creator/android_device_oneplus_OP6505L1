6da976c4f760dbbb5862a4a1cdd4b2e2df3b9bb9
andruha5530-creator <andruha5530@gmail.com>
2026-09-25 07:23:49 +0300

    Run #65: remove unavailable virtual A/B include

--- device.mk
+++ device.mk
@@ -9,24 +9,35 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Qualcomm / GKI recovery support, based on the matching SM7635 recovery
 # trees and the stock OP6505L1 recovery service layout.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Boot control / slot switching.
-PRODUCT_PACKAGES +=     android.hardware.boot-service.qti     android.hardware.boot-service.qti.recovery \
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery \
     android.hardware.health-service.qti_recovery
 
-PRODUCT_PACKAGES_DEBUG +=     bootctl
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
 
 # FastbootD and OTA helpers used by modern A/B recovery trees.
-PRODUCT_PACKAGES +=     fastbootd     update_engine     update_engine_sideload     update_verifier     checkpoint_gc     otapreopt_script
+PRODUCT_PACKAGES += \
+    fastbootd \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc \
+    otapreopt_script
 
-PRODUCT_PACKAGES_DEBUG +=     update_engine_client
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # Qualcomm FBE recovery helpers.
-PRODUCT_PACKAGES +=     qcom_decrypt     qcom_decrypt_fbe
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
 
 # OrangeFox recovery-specific A/B and boot-control behavior.
 OF_AB_DEVICE_WITH_RECOVERY_PARTITION := 1
